57f41270d163a1ea15d23ea3bc32b532d717f628
Vithyaghar <vithyaghar1506@gmail.com>
2024-01-15 11:15:02 +0530

    modified: userLogin(1)

---
+++ frontend/src/pages/userLogin.js
@@ -0,0 +1,34 @@
+
+function LoginPage() {
+    const username = " ", password= " ";
+
+    return(
+        <LoginPage>
+            <div class="container">
+                <div class="wrapper">
+                    <div class="top">
+                        <div class="left">
+                            <div class="leftTop">
+
+                            </div>
+                            <div class="leftBottom">
+
+                            </div>
+                        </div>
+                        <div class="center">
+
+                        </div>
+                        <div class="right">
+
+                        </div>
+                    </div>
+                    <div class="bottom">
+
+                    </div>
+                </div>
+            </div>
+        </LoginPage>
+    );
+}
+
+export default LoginPage;
